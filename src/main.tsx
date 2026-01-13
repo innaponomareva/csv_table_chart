53625f231f43656203cf52dc21c956c0943919f3
Inna Ponomareva <73798843+innaponomareva@users.noreply.github.com>
2026-01-13 02:36:00 +0100

    update bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import './styles/table.css';
 import './styles/chart.css';
 import App from './App';
 import { DataState } from './context/data/dataState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
